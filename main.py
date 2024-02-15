dab2ce69b51b40a167f5ba6fffaa10a49677619c
BhanuTej14 <bhanutej12345678@gmail.com>
2024-02-15 13:25:32 +0530

    Leap Year Code

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-for i in range(1,11):
-    if i%2==0:
-        print(i)
-        print("It is not a odd number")
-    else:
-        print(i)
-        print("It is odd Number")
+year = int(input("Enter a year: "))
+if year%4 == 0 and (year%100!=0 or year%400 == 0):
+    print(year," is a leap year")
+else:
+    print(year," is not a leap year")
    
