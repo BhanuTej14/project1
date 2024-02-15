cee0f2139b06837f956e47b217515f19cebc6667
BhanuTej14 <bhanutej12345678@gmail.com>
2024-02-15 13:09:33 +0530

    this is our first file

---
+++ main.py
@@ -0,0 +1,10 @@
+for i in range(1,11):
+    if i%2==0:
+        print(i)
+        print("It is not a odd number")
+    else:
+        print(i)
+        print("It is odd Number")
+    elif i%i==0 & i%2!=0:
+        print ("i")
+        print("it is a prime number")
